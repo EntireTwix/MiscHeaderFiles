df1a3c80869935cc421d3a3d75613528a90d6b73
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-03-30 02:04:04 -0700

    Deleted sort.h
    was kinda useless

--- sort.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <algorithm>
-#include <vector>
-
-//always add as inhereted by doing virtual ISortable so that it wont break
-template <typename T>
-class ISortable {
-    virtual bool operator<(const T& instance) = 0;
-};
