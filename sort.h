1ac4d78e396efce8db9f458dae2a97bfe641bd11
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-03-30 01:57:33 -0700

    Updated sort.h
    removed operator= pure virtual from sortable interface

--- sort.h
+++ sort.h
@@ -7,7 +7,6 @@ template <typename T>
 class ISortable {
     virtual bool operator<(const T& instance) = 0;
     virtual bool operator>(const T& instance) = 0;
-    virtual bool operator==(const T& instance) = 0;
 };
 
     template <typename T>
