cb95ccfc5523ae67029e41e4ec4b5990cffed94d
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-04-13 17:26:35 -0700

    fixed point.h
    constructor and operator<< we're semi broken

--- point.h
+++ point.h
@@ -8,7 +8,7 @@ class Point
 private:
 	double* members = nullptr;
 public:
-	Point() noexcept { if(size() > 0 ) members = new double[size()]; }
+	Point() noexcept { if (size() > 0) members = new double[size()]{ 0 }; }
 	Point(std::initializer_list<double> list)
 	{
 		if (list.size() > size()) throw std::invalid_argument("list size can not be greater then size of Point");
@@ -21,17 +21,12 @@ public:
 				++i;
 			}
 		}
-		if (size() > 0) members = new double[size()]{ 0 };
-		for (int i = 0; i < list.size(); ++i)
-		{
-			members[i] = p[i];
-		}
-		delete[] p;
+		if (size() > 0) members = std::move(p);
 	}
-	explicit Point(std::vector<double> list)
+	explicit Point(const std::vector<double>& list)
 	{
 		if (list.size() > size()) std::invalid_argument("list size can not be greater then size of Point");
-		if (size() > 0) members = new double[size()]{ 0 };
+		if (size() > 0) members = new double[size()];
 		for (int i = 0; i < list.size(); ++i)
 		{
 			members[i] = list[i];
@@ -42,22 +37,27 @@ public:
 	void operator=(Point& p) noexcept { this->members = std::copy(p.members); this->sz = p.sz; }
 	~Point() { delete[] this->members; }
 	
-	friend std::ostream& operator<<(std::ostream& os, const Point& p) noexcept
-	{
-		for (double* d = p.members; *d; ++d)
-		{
-			os << *d << ' ';
-		}
-		os << '\n';
-		return os;
-	}
 	double& operator[](int pos)
 	{
 		if ((pos < 0) || (pos > size())) throw std::out_of_range("cant be negative or greater then size()");
-		
+
 		return members[pos];
 	}
+
 	constexpr unsigned size() const { if (sz < 0) return NULL; return sz; }
+
+	template <int I>
+	friend std::ostream& operator<<(std::ostream& os, const Point<I>& p) noexcept
+	{
+		os << "{ ";
+		for (int i = 0; i < p.size(); ++i)
+		{
+			os << p.members[i];
+			if (i != p.size() - 1) os << ", ";
+		}
+		os << " }\n";
+		return os;
+	}
 };
 
 using Point2 = Point<2>;
