4d0157f1671023ee4c6cf3b04c0dac4805f2f4f8
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-04-14 15:01:56 -0700

    Update point.h

--- point.h
+++ point.h
@@ -2,17 +2,16 @@
 #include <ostream>
 #include <vector>
 
-template <int sz>
 class Point
 {
 private:
 	float* members = nullptr;
+	int sz = 0;
 public:
-	Point() noexcept { if (size() > 0) members = new float[size()]{ 0 }; }
+	Point(int sz) noexcept { this->sz = sz; if (size() > 0) members = new float[size()]{ 0 }; }
 	Point(std::initializer_list<float> list)
 	{
-		if (list.size() > size()) throw std::invalid_argument("list size can not be greater then size of Point");
-		float* p = new float[size()]{ 0 };
+		float* p = new float[list.size()]{ 0 };
 		{
 			int i = 0;
 			for (float d : list)
@@ -21,37 +20,55 @@ public:
 				++i;
 			}
 		}
-		if (size() > 0) members = std::move(p);
-	}
-	explicit Point(const std::vector<float>& list)
-	{
-		if (list.size() > size()) throw std::invalid_argument("list size can not be greater then size of Point");
-		if (size() > 0) members = new float[size()]{ 0 };
-		for (int i = 0; i < list.size(); ++i)
+		members = new float[list.size()];
+		sz = list.size();
+		if (list.size() > 0)
 		{
-			members[i] = (float)list[i];
+			for (int i = 0; i < list.size(); ++i) members[i] = float(p[i]);
 		}
+		delete[] p;
 	}
-	template <int I>
-	explicit Point(Point<I>&& p) noexcept { members = std::move(p.members); sz = p.sz; p.sz = 0; }
-	
-	template <int I>
-	void operator=(Point<I>&& p) noexcept { members = std::move(p.members); sz = p.sz; p.sz = 0; }
-	
-	template <int I>
-	void operator=(const Point<I>& p) noexcept { members = std::copy(p.members); sz = p.sz; }
+	Point& operator=(std::initializer_list<float> list) 
+	{ 
+		if (list.size() > size()) throw std::invalid_argument("list size can not be greater then size of Point");
+		float* p = new float[list.size()]{ 0 };
+		{
+			int i = 0;
+			for (float d : list)
+			{
+				p[i] = d;
+				++i;
+			}
+		}
+		if(members == nullptr) members = new float[size()]{ 0 };
+		if (size() > 0)
+		{
+			for (int i = 0; i < list.size(); ++i) members[i] = float(p[i]);
+		}
+		delete[] p;
+		return *this; 
+	};
+	Point(const Point& p) noexcept
+	{
+		members = new float[p.sz];
+		for (int i = 0; i < p.sz; ++i)
+		{
+			members[i] = p.members[i];
+		}
+		sz = p.sz;
+	}
+
 	~Point() { delete[] members; }
 	
 	void ApplyFunction(void (*func)(float&)) { for (int i = 0; i < size(); ++i) func(members[i]); }
-	constexpr unsigned size() const { if (sz < 0) return NULL; return sz; }
+	constexpr unsigned size() const { if (sz < 0) return 1; return sz; }
 	float& operator[](int pos)
 	{
 		if ((pos < 0) || (pos > size())) throw std::out_of_range("cant be negative or greater then size()");
 		return members[pos];
 	}
 
-	template <int I>
-	friend std::ostream& operator<<(std::ostream& os, const Point<I>& p) noexcept
+	friend std::ostream& operator<<(std::ostream& os, const Point& p) noexcept
 	{
 		os << "{ ";
 		for (int i = 0; i < p.size(); ++i)
@@ -64,5 +81,4 @@ public:
 	}
 };
 
-using Point2 = Point<2>;
-using Point3 = Point<3>;
+//TO-DO: move constructors
