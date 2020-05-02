918848dc33125ed5c6fa54725b8083b7b2453ad5
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-05-02 15:29:31 -0700

    Update infinite_int.h

--- infinite_int.h
+++ infinite_int.h
@@ -40,14 +40,14 @@ template <size_t numberOfBits = 8>
         public:
             //constructors
             InfiniteInt() { bits.resize(numberOfBits); }
-            InfiniteInt(unsigned long long int x) : InfiniteInt()
+            explicit InfiniteInt(unsigned long long x) : InfiniteInt()
             {
                 for(unsigned long long int i = 0; i < x; ++i)
                 {
                     IncrementBit(0);
                 }
             }
-            InfiniteInt operator=(unsigned long long int x) : InfiniteInt()
+            InfiniteInt operator=(unsigned long long x)
             {
                 for(unsigned long long int i = 0; i < x; ++i)
                 {
