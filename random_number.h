48350e67ed574c5952467cfc40d8cc3bd87d9cca
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-05-31 01:57:35 -0700

    Update random_number.h

--- random_number.h
+++ random_number.h
@@ -5,13 +5,9 @@
 
 int findPrecision(double x) {
 	assert(x >= 0);
-	int i = 0;
-	for (; (x * pow(10, i)) > ((int)(x * pow(10, i))); ++i);
-	
-	++i;
-	return i;
+	for (int i = 0; (x * pow(10, i)) > ((int)(x * pow(10, i))); ++i) { }
+	return ++i;
 }
-
 
 /* lowest , highest (both positive)*/
 double pickNumber(double a, double b) {
@@ -19,21 +15,6 @@ double pickNumber(double a, double b) {
 	assert(a < b);
 
 	short increment;
-	if (findPrecision(a) > findPrecision(b)) {
-		//if A is more precise
-		increment = pow(10, findPrecision(a));
-	}
-	else {
-		//if B is more precise
-		increment = pow(10, findPrecision(b));
-	}
-
-	if(increment == 0){
-		//random number that is between 0 and (highest-lowest)+1, then add lowest to the result
-		return (double)((rand() % ((int)((b - a)  + 1))) + a);
-	}
-	else {
-		//otherwise add precision to random result
-		return (double)( (rand() % ((int)( ((b-a) * increment) + 1)) ) + (a*increment) )/increment;
-	}
+	(findPrecision(a) > findPrecision(b))?increment = pow(10, findPrecision(a)):increment = pow(10, findPrecision(b));
+	(!increment)? return (double)((rand() % ((int)((b - a)  + 1))) + a):return (double)( (rand() % ((int)( ((b-a) * increment) + 1)) ) + (a*increment) )/increment;
 }
