3b7c39c71ad9b5674c61b1677f5472022a8af636
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-06-17 22:21:33 -0700

    replaced some convertible_to constraints with same_as

--- generics.h
+++ generics.h
@@ -8,36 +8,39 @@
 template <typename T>
 concept EqualityComparable = requires(T a, T b) 
 {
-    { a == b } -> std::convertible_to<bool>;
-    { a != b } -> std::convertible_to<bool>;
+    { a == b } -> std::same_as<bool>;
+    { a != b } -> std::same_as<bool>;
 };
 
 template <typename T>
 concept UnaryOperators = requires(T a)
 {
-    { a++ } -> std::convertible_to<T>;
-    { a-- } -> std::convertible_to<T>;
+    { a++ } -> std::same_as<T>;
+    { a-- } -> std::same_as<T>;
 };
 
 template <typename T, typename T2 = T, typename T3 = T>
 concept ArithmeticOperators = requires(T a, T2 b)
 {
-    { a+b } -> std::convertible_to<T3>;
-    { a-b } -> std::convertible_to<T3>;
-    { a*b } -> std::convertible_to<T3>;
-    { a/b } -> std::convertible_to<T3>;
+    { a+b } -> std::same_as<T3>;
+    { a-b } -> std::same_as<T3>;
+    { a*b } -> std::same_as<T3>;
+    { a/b } -> std::same_as<T3>;
 };
 
 template <typename T>
 concept RelationalOperators = requires(T a, T b)
 {
-    { a<b } -> std::convertible_to<bool>;
-    { a<=b } -> std::convertible_to<bool>;
-    { a>b } -> std::convertible_to<bool>;
-    { a>=b } -> std::convertible_to<bool>;
+    { a<b } -> std::same_as<bool>;
+    { a<=b } -> std::same_as<bool>;
+    { a>b } -> std::same_as<bool>;
+    { a>=b } -> std::same_as<bool>;
 
     requires EqualityComparable<T>;
 };
+
+template <typename T>
+concept Number = std::convertible_to<T, int>;
 
 //lifted functions
 
