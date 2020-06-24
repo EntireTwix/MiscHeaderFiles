2cf49141b8b19d87710c6157061760dfc9cfaa29
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-06-24 10:32:59 -0700

    ditched new approach as it was overcomplicated and buggy

--- random_number.h
+++ random_number.h
@@ -1,52 +1,8 @@
 #pragma once
 #include <random>
-#include <ctime>
 #include <stdexcept>
 
-constexpr unsigned char GetPrecision(double x)
+unsigned Random(unsigned lowest, unsigned highest)
 {
-	unsigned char i = 0;
-	while(x > (int)x)
-	{
-		x*=10;
-		++i;
-	}
-	return i;
+	return ((rand()%(unsigned)(highest))+offset);
 }
-
-class Random final
-{
-private:
-	unsigned lowest = 0, highest = 1, offset = 0;
-public:
-	Random() = default;
-	Random(unsigned a, unsigned b)
-	{
-		if( (a < 0) || (b < 0) ) throw std::invalid_argument("numbers must be positive");
-		//setting highest to the higher of the two, and finding offset
-		if(a == b) throw std::out_of_range("first and second argument must be different");
-		else if(a > b) 
-		{
-			highest = a;
-			lowest = b;
-		}
-		else if(b > a)
-		{
-			highest = b;
-			lowest = a;
-		}
-
-		//setting offset
-		if(lowest != 0)
-		{
-			offset = lowest;
-			lowest = 0;
-			highest -= lowest;
-		}
-	} 
-
-	unsigned get() const 
-	{
-		return (double)((rand()%(int)(highest))+offset);	
-	}
-};
