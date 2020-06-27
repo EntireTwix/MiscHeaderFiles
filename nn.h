c11939023adaaf27394d9ea0f110cbae68da83d6
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-06-27 00:54:02 -0700

    worked on forward prop function

--- nn.h
+++ nn.h
@@ -1,6 +1,7 @@
 #pragma once
+#include <memory>
+#include <thread>
 #include "mat.h"
-#include <vector>
 
 //use std::uniform_real_distribution<>
 
@@ -9,6 +10,19 @@ class NeuralNetwork
 private:
     Mat<double>* layers = nullptr;
     size_t size = 0; //possibly irrelevant variable
+    Mat<double> ForwardProp() const
+    {
+        std::unique_ptr< Mat<double>[] > currentMat = std::make_unique< Mat<double>[]>(size); //smart pointers gang
+        for(size_t i = 0; i < size; ++i)
+        {
+            currentMat[i] = Mat<double>(layers[i].sizeX(), layers[i].sizeY());
+            for(size_t j = 0; j < layers[i].sizeY(); ++j)
+            for(size_t k = 0; k < layers[i].sizeX(); ++k)
+                currentMat[i].at(j, k) = layers[i].at(j, k);
+        }
+        //do stuff
+        return currentMat[size-1];
+    }
 public: 
     template <typename... Params>
     NeuralNetwork(size_t first, Params ...p)
@@ -17,6 +31,7 @@ public:
         layers = new Mat<double>[size];
         layers[0] = Mat<double>(first, 1); 
         AddLayer(1, p...);
+        //set weights
     }
 
     template <typename... Params, Number T>
@@ -27,6 +42,17 @@ public:
         loc+=2;
         if constexpr(sizeof...(p)) AddLayer(loc, p...);
     }
+    template <int sampleSize = 1>
+    Mat<double> ForwardProp() const //returns iteration cost
+    {
+        if constexpr(!sampleSize) throw std::invalid_argument("sample size must be higher then 0");
+        if constexpr(sampleSize > 1) // multi threading condition
+        {
+            //make {sampleSize} amount of threads
+        }
+        else return ForwardProp();
+    }
+    //add back prop
 
     ~NeuralNetwork()
     {
