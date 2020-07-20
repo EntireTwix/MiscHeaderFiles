42678b384a1d3c031b5b574ad4f9f2d542243f4a
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-07-19 20:24:18 -0700

    performance improvements

--- tpool.h
+++ tpool.h
@@ -28,30 +28,21 @@ public:
         for(uint_fast8_t i = 0; i < threadCount; ++i)
             workers[i] = std::thread([this,i](){
                     while(!stopped)
-                    {                        
-                    
+                    {  
+                        {
+                            std::unique_lock<std::mutex> jobsAccess{threadLocks[i]}; //when its this threads turn
+                            jobListener[i].wait(jobsAccess, [this,i](){ return !(jobs[i].empty() || stopped); } );
+                        }
+
+                        if(!paused)
+                        {
                             std::function<void()> job;
-                            //std::cout<<"Thread "<<(int)i+1<<" is running\n";
-                            
-                            {
-                                std::unique_lock<std::mutex> jobsAccess{threadLocks[i]}; //when its this threads turn
-                                //if((!jobs[i].empty() || stopped)) std::cout<<"waiting\n";
-                                jobListener[i].wait(jobsAccess, [this,i](){ return !(jobs[i].empty() || stopped); } );
-                            }
-                        
-                            if(!paused)
-                            {
-                                //std::cout<<"working\n";
-                                job = jobs[i].front();
-                                job();
-                                jobs[i].pop();
-                            }
-                        
-                        //else std::cout<<"Thread "<<(int)i+1<<" is paused\n";
+                            job = jobs[i].front();
+                            job();
+                            jobs[i].pop();
+                        }
                     }
             });
-
-        //std::cout<<(int)threadCount<<" threads spawned\n";
     }
 
     
@@ -60,10 +51,9 @@ public:
         {
             std::unique_lock<std::mutex> jobsAccess{threadLocks[nextIndex]}; //when its this threads turn
             jobs[nextIndex].push(func);
-            jobListener[nextIndex].notify_one();
-            //std::cout<<"jobs notified "<<nextIndex<<'\n';
-            //std::cout<<"job added\n";
         }
+        jobListener[nextIndex].notify_one();
+        
         nextIndex = nextIndex==(threadCount-1)?0:nextIndex+1;
     }
     size_t JobsLeft()
@@ -71,35 +61,25 @@ public:
         size_t sum = 0;
         for(uint_fast8_t i = 0; i < threadCount; ++i)
         {
-            std::unique_lock<std::mutex> jobsAccess{threadLocks[i]}; //when its this threads turn
-            sum += jobs[i].size();
+            {
+                std::unique_lock<std::mutex> jobsAccess{threadLocks[i]}; //when its this threads turn
+                sum += jobs[i].size();
+            }
         }
         return sum;
     }
 
     void start() 
     {
-        for(uint_fast8_t i = 0; i < threadCount; ++i)
-        {
-            std::unique_lock<std::mutex> jobsAccess{threadLocks[i]}; //when its this threads turn
-            paused = false;
-        }
+        paused = false;
     }
     void pause()
     {
-        for(uint_fast8_t i = 0; i < threadCount; ++i)
-        {
-            std::unique_lock<std::mutex> jobsAccess{threadLocks[i]}; //when its this threads turn
-            paused = true;
-        }
+        paused = true;
     }
     void stop()
     {
-        for(uint_fast8_t i = 0; i < threadCount; ++i)
-        {
-            std::unique_lock<std::mutex> jobsAccess{threadLocks[i]}; //when its this threads turn
-            stopped = true;
-        }
+        stopped = true;
     }
     
 
