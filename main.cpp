46406aa10953d035fb0a79bf72d62b5aac16d402
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-07-22 16:44:10 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,8 @@
 #include <windows.h>
 #include "mat.h"
 #include "tpool.h"
-#include <omp.h>
 
 using namespace std::chrono;  
-
-//std::minstd_rand LinearCongruential( ProgStartTime - system_clock::now().time_since_epoch().count() );
-//a.At(i,j) = LinearCongruential();
-//high_resolution_clock::time_point TimePointStart = high_resolution_clock::now();
 
 uint32_t TimeNow = system_clock::now().time_since_epoch().count(); // Fastest way to get epoch time
 
@@ -25,7 +20,7 @@ int main()
 
     Mat<size,size> a;
 
-    for(size_t z = 0; z < 100; ++z)
+    //for(size_t z = 0; z < 100; ++z)
     {
         pool.pause();
         high_resolution_clock::time_point TimePointStart = high_resolution_clock::now();
@@ -42,22 +37,20 @@ int main()
                 //std::cout<<"finished "<<i<<'\n';
             });
         high_resolution_clock::time_point TimePointEnd = high_resolution_clock::now();
-        //std::cout<<pool.JobsLeft()<<" start jobs\n";
-        //std::cout<<"Thread Task Compilation Time done: "<<duration_cast<duration<double>>(TimePointEnd - TimePointStart).count()<<'\n';
+        std::cout<<pool.JobsLeft()<<" start jobs\n";
+        std::cout<<"Thread Task Compilation Time done: "<<duration_cast<duration<double>>(TimePointEnd - TimePointStart).count()<<'\n';
 
         TimePointStart = high_resolution_clock::now();
         pool.start();
         while(pool.JobsLeft()) { /*std::cout<<pool.JobsLeft()<<" tasks\n";*/ }
         TimePointEnd = high_resolution_clock::now();
-        //std::cout<<pool.JobsLeft()<<" end jobs\n";
+        std::cout<<pool.JobsLeft()<<" end jobs\n";
 
         std::cout<<"Runtime done: "<<duration_cast<duration<double>>(TimePointEnd - TimePointStart).count()<<"\n";
         sum+=duration_cast<duration<double>>(TimePointEnd - TimePointStart).count();
         //std::cout<<a;
     }
     std::cout<<(sum/100)*-1<<'\n';
-    
-    
 
     return 0;
 }
