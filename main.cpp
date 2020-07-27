905e528065a878869f739adfe12306a11d0d00a5
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-07-27 14:22:36 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
+#include <windows.h>
 #include <random>
 #include <chrono>
-#include <windows.h>
-#include "mat.h"
 #include "tpool.h"
+#include "mat.h"
+
+#define activation VK_XBUTTON2
+
 
 using namespace std::chrono;  
 
 uint32_t TimeNow = system_clock::now().time_since_epoch().count(); // Fastest way to get epoch time
 
-#define logical_cores 16
-#define size 10000
-#define increment size/logical_cores
+auto logical_cores = std::thread::hardware_concurrency();
+auto size = 10000;
+auto increment = size/logical_cores;
 
 int main()
 {
-    ThreadPool pool(logical_cores);
+
+    ThreadPool pool;
     double sum = 0;
 
-    Mat<size,size> a;
+    Mat a(size, size);
 
-    //for(size_t z = 0; z < 100; ++z)
+    for(size_t z = 0; z < 100; ++z)
     {
         pool.pause();
         high_resolution_clock::time_point TimePointStart = high_resolution_clock::now();
@@ -37,20 +41,19 @@ int main()
                 //std::cout<<"finished "<<i<<'\n';
             });
         high_resolution_clock::time_point TimePointEnd = high_resolution_clock::now();
-        std::cout<<pool.JobsLeft()<<" start jobs\n";
-        std::cout<<"Thread Task Compilation Time done: "<<duration_cast<duration<double>>(TimePointEnd - TimePointStart).count()<<'\n';
+        //std::cout<<pool.JobsLeft()<<" start jobs\n";
+        //std::cout<<"Thread Task Compilation Time done: "<<duration_cast<duration<double>>(TimePointEnd - TimePointStart).count()<<'\n';
 
         TimePointStart = high_resolution_clock::now();
         pool.start();
         while(pool.JobsLeft()) { /*std::cout<<pool.JobsLeft()<<" tasks\n";*/ }
         TimePointEnd = high_resolution_clock::now();
-        std::cout<<pool.JobsLeft()<<" end jobs\n";
+        //std::cout<<pool.JobsLeft()<<" end jobs\n";
 
         std::cout<<"Runtime done: "<<duration_cast<duration<double>>(TimePointEnd - TimePointStart).count()<<"\n";
         sum+=duration_cast<duration<double>>(TimePointEnd - TimePointStart).count();
         //std::cout<<a;
     }
     std::cout<<(sum/100)*-1<<'\n';
-
-    return 0;
+    return 0;    
 }
