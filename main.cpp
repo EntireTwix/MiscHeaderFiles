d2cdbc17e4aa6eed816c4641de58a056db9c5e8e
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-08-06 12:15:05 -0700

    added better output, changed for new threadpool

--- main.cpp
+++ main.cpp
@@ -2,56 +2,58 @@
 #include <windows.h>
 #include <random>
 #include <chrono>
+#include <iomanip>
 #include "tpool.h"
 #include "mat.h"
 
-
-using namespace std::chrono;  
+using namespace std::chrono;
 
 uint32_t TimeNow = system_clock::now().time_since_epoch().count(); // Fastest way to get epoch time
 
-auto logical_cores = std::thread::hardware_concurrency();
+auto logical_cores = 12;
 auto size = 10000;
-auto increment = size/logical_cores;
+auto increment = size / logical_cores;
+auto overflow = size%logical_cores;
 
 int main()
 {
 
-    ThreadPool pool;
+    ThreadPool<12> pool;
     double sum = 0;
 
     Mat a(size, size);
 
-    for(size_t z = 0; z < 100; ++z)
+    for (size_t z = 0; z < 100; ++z)
     {
-        pool.pause();
+        pool.Pause();
         high_resolution_clock::time_point TimePointStart = high_resolution_clock::now();
-        for(size_t i = 0; i < size; i+=increment)
-            pool.AddTask([&a,i](){
-                //std::cout<<"working "<<i<<'\n';
-                for(size_t j = 0; j < size; ++j)
-                for(size_t k = i; k < i+increment; ++k)
+        for (size_t i = 0; i < size-overflow; i += increment)
+            pool.AddTask([&a, i]() {
+            //std::cout<<"working "<<i<<'\n';
+            for (size_t j = 0; j < size; ++j)
+                for (size_t k = i; k < i + increment; ++k)
                 {
-                    std::minstd_rand LinearCongruential( TimeNow - system_clock::now().time_since_epoch().count() );
-                    
-                    a.At(k,j) = LinearCongruential();
+                    std::minstd_rand LinearCongruential(TimeNow - system_clock::now().time_since_epoch().count());
+
+                    a.At(k, j) = LinearCongruential();
                 }
-                //std::cout<<"finished "<<i<<'\n';
-            });
+            //std::cout<<"finished "<<i<<'\n';
+                });
         high_resolution_clock::time_point TimePointEnd = high_resolution_clock::now();
-        //std::cout<<pool.JobsLeft()<<" start jobs\n";
+        std::cout << pool.Jobs() << " start jobs\n";
         //std::cout<<"Thread Task Compilation Time done: "<<duration_cast<duration<double>>(TimePointEnd - TimePointStart).count()<<'\n';
-
         TimePointStart = high_resolution_clock::now();
-        pool.start();
-        while(pool.JobsLeft()) { /*std::cout<<pool.JobsLeft()<<" tasks\n";*/ }
+        pool.Start();
+        while (pool.Jobs())
+        {
+        }
         TimePointEnd = high_resolution_clock::now();
-        //std::cout<<pool.JobsLeft()<<" end jobs\n";
+        std::cout << pool.Jobs() << " end jobs\n";
 
-        std::cout<<"Runtime done: "<<duration_cast<duration<double>>(TimePointEnd - TimePointStart).count()<<"\n";
-        sum+=duration_cast<duration<double>>(TimePointEnd - TimePointStart).count();
+        std::cout << std::setprecision(5) << std::fixed << "Runtime done: " << duration_cast<duration<double>>(TimePointEnd - TimePointStart).count() << "\n\n";
+        sum += duration_cast<duration<double>>(TimePointEnd - TimePointStart).count();
         //std::cout<<a;
     }
-    std::cout<<(sum/100)*-1<<'\n';
-    return 0;    
+    std::cout << (sum / 100) * -1 << '\n';
+    return 0;
 }
