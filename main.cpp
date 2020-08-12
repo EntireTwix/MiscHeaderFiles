207f5a2b7b3a028ae557a323e435921ace50de2f
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-08-11 18:31:42 -0700

    minor edits

--- main.cpp
+++ main.cpp
@@ -10,20 +10,19 @@ using namespace std::chrono;
 
 uint32_t TimeNow = system_clock::now().time_since_epoch().count(); // Fastest way to get epoch time
 
-auto logical_cores = 12;
+auto logical_cores = std::thread::hardware_concurrency();
 auto size = 10000;
 auto increment = size / logical_cores;
 auto overflow = size%logical_cores;
 
 int main()
 {
-
-    ThreadPool<12> pool;
+    ThreadPool pool;
     double sum = 0;
 
-    Mat a(size, size);
+    Mat<uint_fast8_t> a(size, size);
 
-    for (size_t z = 0; z < 100; ++z)
+    for (int z = 0; z < 10; ++z)
     {
         pool.Pause();
         high_resolution_clock::time_point TimePointStart = high_resolution_clock::now();
@@ -34,13 +33,13 @@ int main()
                 for (size_t k = i; k < i + increment; ++k)
                 {
                     std::minstd_rand LinearCongruential(TimeNow - system_clock::now().time_since_epoch().count());
-
-                    a.At(k, j) = LinearCongruential();
+                    a.At(k, j) = (uint_fast8_t)LinearCongruential();
                 }
             //std::cout<<"finished "<<i<<'\n';
                 });
         high_resolution_clock::time_point TimePointEnd = high_resolution_clock::now();
         std::cout << pool.Jobs() << " start jobs\n";
+
         //std::cout<<"Thread Task Compilation Time done: "<<duration_cast<duration<double>>(TimePointEnd - TimePointStart).count()<<'\n';
         TimePointStart = high_resolution_clock::now();
         pool.Start();
@@ -54,6 +53,6 @@ int main()
         sum += duration_cast<duration<double>>(TimePointEnd - TimePointStart).count();
         //std::cout<<a;
     }
-    std::cout << (sum / 100) * -1 << '\n';
+    std::cout << (sum / 10) * -1 << '\n';
     return 0;
 }
