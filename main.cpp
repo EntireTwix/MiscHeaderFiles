189561581354f50360710f8b9a36d772af5c4f57
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-08-14 23:43:42 -0700

    was skipping every other

--- main.cpp
+++ main.cpp
@@ -16,19 +16,18 @@ auto size = 10000;
 auto increment = size / logical_cores;
 auto overflow = size % logical_cores;
 
-inline void Random16(unsigned char *pAt, __m128i &timeValues, __m128i &constantValues, __m128i &res)
+void Random16(unsigned char *pAt, __m256i &timeValues, __m256i &constantValues, __m256i &res)
 {
     for (int i = 0; i < 4; ++i)
     {
         timeValues[i] = (system_clock::now().time_since_epoch().count() - TimeNow); //4 x 16 bit values
     }
 
-    constantValues = _mm_set1_epi16(48271);
-    res = _mm_madd_epi16(timeValues, constantValues);
-
+    constantValues = _mm256_set1_epi64x(48271);
+    res = _mm256_mul_epi32(timeValues, constantValues);
     for (int i = 0; i < 4; ++i)
     {
-        pAt[i] = res[i] % 64;
+        pAt[i] = res[i];
     }
 }
 
@@ -44,19 +43,21 @@ int main()
         pool.Pause();
         high_resolution_clock::time_point TimePointStart = high_resolution_clock::now();
         for (size_t i = 0; i < size - overflow; i += increment)
+        {
             pool.AddTask([&a, i]() {
-                __m128i t, c, r;
+                __m256i t, c, r;
 
                 for (size_t j = 0; j < size; ++j)
                 {
                     for (size_t k = i; k < i + increment; k += 4)
                     {
-                        if (k + 4 < size)
+                        if ((k + 4) < size)
                             Random16(a.AtP(k, j), t, c, r);
                     }
                 }
                 //std::cout<<"finished "<<i<<'\n';
             });
+        }
         high_resolution_clock::time_point TimePointEnd = high_resolution_clock::now();
         std::cout << pool.Jobs() << " start jobs\n";
 
@@ -74,5 +75,6 @@ int main()
         //std::cout << a;
     }
     std::cout << (sum / 100) * -1 << '\n';
+    std::cin.get();
     return 0;
 }
