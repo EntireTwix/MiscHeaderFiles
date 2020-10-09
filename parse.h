c4cda6d954a10c8e9c6897cd98d981aa8c3932a9
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-09 09:28:58 -0700

    :racehorse: stringview for performance

--- parse.h
+++ parse.h
@@ -4,87 +4,92 @@
 #include <sstream>
 #include <exception>
 
-class TextParser final 
+class TextParser final
 {
 private:
-	static std::vector<std::string> ParseLine(const std::string& line, char sep) 
+	static std::vector<std::string> ParseLine(const std::string &line, char sep)
 	{
 		std::stringstream ss(line);
 		std::vector<std::string> result;
 		std::string temp;
-		while (getline(ss, temp, sep)) 
+		while (getline(ss, temp, sep))
 		{
 			result.push_back(temp);
 		}
 		return result;
 	}
-	
+
 public:
-	static std::vector< std::vector<std::string> > Parse(const std::string& location, char sep, unsigned long numOfVars) 
+	static std::vector<std::vector<std::string>> Parse(const std::string_view &location, char sep, unsigned long numOfVars)
 	{
 		std::ifstream inputFile(location);
-		std::vector<std::vector<std::string> >result;
+		std::vector<std::vector<std::string>> result;
 		std::string line;
-		while(getline(inputFile, line) ) 
+		while (getline(inputFile, line))
 		{
-			if (numOfVars != 0) 
+			if (numOfVars != 0)
 			{
-				if (ParseLine(line, sep).size() != numOfVars) throw new std::invalid_argument((i + 1) + "line has an invalid amount of parameters");
+				if (ParseLine(line, sep).size() != numOfVars)
+					throw new std::invalid_argument((i + 1) + "line has an invalid amount of parameters");
 			}
 			result.push_back(ParseLine(line, sep));
 		}
 		inputFile.close();
 		return result;
 	}
-	static std::vector< std::vector<std::string> > Parse(const std::string& location, char sep) 
+	static std::vector<std::vector<std::string>> Parse(const std::string_view &location, char sep)
 	{
 		std::ifstream inputFile(location);
-		std::vector<std::vector<std::string> >result;
+		std::vector<std::vector<std::string>> result;
 		std::string line;
-		while (getline(inputFile, line) ) result.push_back(ParseLine(line, sep));
+		while (getline(inputFile, line))
+			result.push_back(ParseLine(line, sep));
 		inputFile.close();
 		return result;
 	}
-	static std::vector<std::string> Parse(const std::string& location)
+	static std::vector<std::string> Parse(const std::string_view &location)
 	{
 		std::ifstream inputFile(location);
-		std::vector<std::string>result;
+		std::vector<std::string> result;
 		std::string line;
-		while(getline(inputFile, line, '\n') ) result.push_back(line);
+		while (getline(inputFile, line, '\n'))
+			result.push_back(line);
 		inputFile.close();
 		return result;
 	}
 
-	static void Save(const std::string& location, std::vector<std::vector<std::string>> vec, char sep, unsigned short int thresholdSize) 
+	static void Save(const std::string_view &location, std::vector<std::vector<std::string>> vec, char sep, unsigned short int thresholdSize)
 	{
 		std::ofstream outputFile(location);
-		for (int i = 0; i < vec.size(); ++i) 
+		for (int i = 0; i < vec.size(); ++i)
 		{
-			for (int j = 0; j < vec[i].size(); ++j) 
+			for (int j = 0; j < vec[i].size(); ++j)
 			{
 				outputFile << vec[i][j];
-				if (j != vec[i].size() - 1) outputFile << sep;
+				if (j != vec[i].size() - 1)
+					outputFile << sep;
 			}
-			if (vec[i].size() >= thresholdSize) outputFile << '\n';
+			if (vec[i].size() >= thresholdSize)
+				outputFile << '\n';
 		}
 		outputFile.close();
 	}
-	template<typename T>
-	static void Save(const std::string& location, std::vector<T> vec, char sep) 
+	template <typename T>
+	static void Save(const std::string_view &location, std::vector<T> vec, char sep)
 	{
 		std::ofstream outputFile(location);
-		for (int i = 0; i < vec.size(); ++i) 
+		for (int i = 0; i < vec.size(); ++i)
 		{
 			outputFile << vec[i] << sep;
 		}
 		outputFile.close();
 	}
-	
-	template<typename T>
-	static void Save(const std::string& location, std::vector<T> vec) 
+
+	template <typename T>
+	static void Save(const std::string_view &location, std::vector<T> vec)
 	{
 		std::ofstream outputFile(location);
-		for (int i = 0; i < vec.size(); ++i) 
+		for (int i = 0; i < vec.size(); ++i)
 		{
 			outputFile << vec[i];
 		}
