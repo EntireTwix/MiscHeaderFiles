2e3d0907614c1c03b9cf8c472f339c95981fad7e
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-14 13:40:46 -0700

    :bug: troubleshooting vec push seg fault

--- point.h
+++ point.h
@@ -9,11 +9,11 @@ template <typename T, size_t sz>
 requires EqualityComparable<T> class Point
 {
 protected:
-    std::array<T, sz> members;
+    T members[sz];
 
 public:
     Point() = default;
-    Point(auto head, auto... args) : members{head, args...} { static_assert(sizeof...(args) + 1 == sz, "args initilizing point must be the same size"); }
+    Point(auto... args) : members{args...} { static_assert(sizeof...(args) == sz, "args initilizing point must be the same size"); }
 
     T &operator[](size_t index)
     {
@@ -119,7 +119,7 @@ template <typename T, size_t sz>
 struct UnsafeVec : public Point<T, sz>
 {
     UnsafeVec() = default;
-    UnsafeVec(auto head, auto... args) : Point<T, sz>(head, args...) {}
+    UnsafeVec(auto... args) : Point<T, sz>(args...) {}
     std::array<T, sz> operator+(const UnsafeVec<T, sz> &p) const
     {
         std::array<T, sz> res;
@@ -169,7 +169,7 @@ struct RGB : public UnsafeVec<uint_fast8_t, 3>
     {
         members[0] = members[1] = members[2] = 0;
     }
-    RGB(auto head, auto &&... args) : UnsafeVec<uint_fast8_t, 3>(head, args...) {}
+    RGB(auto... args) : UnsafeVec<uint_fast8_t, 3>(args...) {}
     RGB(uint_fast8_t R, uint_fast8_t G, uint_fast8_t B)
     {
         members[0] = R;
@@ -198,7 +198,7 @@ struct RGBA : public UnsafeVec<uint_fast8_t, 4>
     {
         members[0] = members[1] = members[2] = 0;
     }
-    RGBA(auto head, auto &&... args) : UnsafeVec<uint_fast8_t, 4>(head, args...) {}
+    RGBA(auto... args) : UnsafeVec<uint_fast8_t, 4>(args...) {}
     RGBA(uint_fast8_t R, uint_fast8_t G, uint_fast8_t B)
     {
         members[0] = R;
