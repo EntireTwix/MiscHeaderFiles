7d8cc5af27db9b02062a1d8e85a7724a126899b1
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-20 22:10:19 -0700

    :sparkles: std::array constructor

--- point.h
+++ point.h
@@ -13,7 +13,14 @@ protected:
 
 public:
     Point() = default;
-    Point(auto head, auto &&... args) : members{head, args...} { static_assert(sizeof...(args) + 1 == sz, "args initilizing point must be the same size"); }
+    Point(const std::array<T, sz> &arr)
+    {
+        for (size_t i = 0; i < sz; ++i)
+        {
+            this->members[i] = arr[i];
+        }
+    }
+    explicit Point(auto head, auto &&... args) : members{head, args...} { static_assert(sizeof...(args) + 1 == sz, "args initilizing point must be the same size"); }
 
     T &operator[](size_t index)
     {
@@ -119,7 +126,9 @@ template <typename T, size_t sz>
 struct UnsafeVec : public Point<T, sz>
 {
     UnsafeVec() = default;
-    UnsafeVec(auto head, auto &&... args) : Point<T, sz>(head, args...) {}
+    UnsafeVec(const std::array<T, sz> &arr) : Point<T, sz>(arr) {}
+    explicit UnsafeVec(auto head, auto &&... args) : Point<T, sz>(head, args...) {}
+
     std::array<T, sz> operator+(const UnsafeVec<T, sz> &p) const
     {
         std::array<T, sz> res;
@@ -163,62 +172,10 @@ template <typename T, size_t sz>
 requires ArithmeticOperators<T> using SafeVec = UnsafeVec<T, sz>;
 
 //RGB
-struct RGB : public UnsafeVec<uint_fast8_t, 3>
-{
-    RGB()
-    {
-        members[0] = members[1] = members[2] = 0;
-    }
-    RGB(auto head, auto &&... args) : UnsafeVec<uint_fast8_t, 3>(head, args...) {}
-    RGB(uint_fast8_t R, uint_fast8_t G, uint_fast8_t B)
-    {
-        members[0] = R;
-        members[1] = G;
-        members[2] = B;
-    }
-
-    uint_fast8_t &r() { return members[0]; }
-    uint_fast8_t &g() { return members[1]; }
-    uint_fast8_t &b() { return members[2]; }
-
-    uint_fast8_t r() const { return members[0]; }
-    uint_fast8_t g() const { return members[1]; }
-    uint_fast8_t b() const { return members[2]; }
-
-    friend std::ostream &operator<<(std::ostream &os, const RGB &p)
-    {
-        return os << '(' << (int)p.members[0] << ", " << (int)p.members[1] << ", " << (int)p.members[2] << ')';
-    }
-};
+using RGB = UnsafeVec<uint8_t, 3>;
 
 //RGBA
-struct RGBA : public UnsafeVec<uint_fast8_t, 4>
-{
-    RGBA()
-    {
-        members[0] = members[1] = members[2] = 0;
-    }
-    RGBA(auto head, auto &&... args) : UnsafeVec<uint_fast8_t, 4>(head, args...) {}
-    RGBA(uint_fast8_t R, uint_fast8_t G, uint_fast8_t B)
-    {
-        members[0] = R;
-        members[1] = G;
-        members[2] = B;
-    }
-
-    uint_fast8_t &r() { return members[0]; }
-    uint_fast8_t &g() { return members[1]; }
-    uint_fast8_t &b() { return members[2]; }
-
-    uint_fast8_t r() const { return members[0]; }
-    uint_fast8_t g() const { return members[1]; }
-    uint_fast8_t b() const { return members[2]; }
-
-    friend std::ostream &operator<<(std::ostream &os, const RGBA &p)
-    {
-        return os << '(' << (int)p.members[0] << ", " << (int)p.members[1] << ", " << (int)p.members[2] << ", " << (int)p.members[3] << ')';
-    }
-};
+using RGBA = UnsafeVec<uint8_t, 4>;
 
 template <typename T, size_t sz>
 using Vec = SafeVec<T, sz>;
