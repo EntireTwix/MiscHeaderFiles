840b0ea97cc286db3b60c80ad786554c9cec860b
William Katz <willkatz.dev@gmail.com>
2021-02-17 00:33:48 -0800

    :racehorse: moved timing to the asyncfor_each func

--- MatrixChallenge.cpp
+++ MatrixChallenge.cpp
@@ -1,24 +1,15 @@
 #include <iostream>
-#include <chrono>
-#include <iomanip>
 #include "mat.hpp"
 #include "tpool.hpp"
 
-using namespace std::chrono;
-
-static uint32_t TimeNow = system_clock::now().time_since_epoch().count(); // Fastest way to get epoch time
-
 int main()
 {
-    static ThreadPool<4> engine;
+    static ThreadPool<8> engine;
     static Mat<uint_fast8_t> data(10000, 10000);
     while (1)
     {
-        high_resolution_clock::time_point TimePointStart = high_resolution_clock::now();
         asyncfor_each(
             data.begin(), data.end(), [](uint_fast8_t &x) { x = ((system_clock::now().time_since_epoch().count() - TimeNow) * 48271) % 256; }, engine);
-        high_resolution_clock::time_point TimePointEnd = high_resolution_clock::now();
-        std::cout << std::setprecision(5) << std::fixed << "Runtime done: " << duration_cast<duration<double>>(TimePointEnd - TimePointStart).count() << '\n';
     }
     return 0;
 }
