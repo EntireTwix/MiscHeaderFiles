3714a9a9fb43bfeff02993851b37c9f63b5799f1
William Katz <willkatz.dev@gmail.com>
2021-02-20 22:53:11 -0800

    heavily simplified copy_fast

--- meta.hpp
+++ meta.hpp
@@ -1,19 +1,9 @@
 #include <type_traits>
 
-template <typename T, bool is_trivially_copyable>
-struct copy_fast;
-
 template <typename T>
-struct copy_fast<T, false>
+struct copy_fast : std::conditional<std::is_trivially_copyable_v<T>, T, const T &>
 {
-    using type = const T &;
 };
 
 template <typename T>
-struct copy_fast<T, true>
-{
-    using type = T;
-};
-
-template <typename T>
-using copy_fast_t = copy_fast<T, std::is_trivially_copyable_v<T>>::type;
+using copy_fast_t = typename copy_fast<T>::type;
