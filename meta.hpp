9bebe1ac0a0df28bc8dab860f53ae9c2bea2cd7d
William Katz <willkatz.dev@gmail.com>
2021-03-09 19:02:03 -0800

    :fire: moved to utils.hpp in OptimizedHeaders repo

--- meta.hpp
+++
@@ -1,9 +0,0 @@
-#include <type_traits>
-
-template <typename T>
-struct copy_fast : std::conditional<std::is_trivially_copyable_v<T>, T, const T &>
-{
-};
-
-template <typename T>
-using copy_fast_t = typename copy_fast<T>::type;
